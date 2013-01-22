44493730070e98092a47d74e5bf7dc13e546df81
Mr Bumpy <MrPropper@AhoraSeLlama.DonLimpio>
2013-01-22 21:53:35 +0100

    Subiendo

---
+++ RemindMe/src/remind/me/RemindAllActivity.java
@@ -0,0 +1,15 @@
+package remind.me;
+
+
+
+import android.os.Bundle;
+
+public class RemindAllActivity extends RemindActivity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.all);
+        
+    }
+}
---
+++ RemindMe/src/remind/me/RemindNewActivity.java
@@ -0,0 +1,14 @@
+package remind.me;
+
+
+import android.os.Bundle;
+
+public class RemindNewActivity extends RemindActivity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.new_edit);
+        
+    }
+}
